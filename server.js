52c01c90931ba747801c3a5c1f4f6751572e9c02
Alexander Sabani <sabani@loidl-consulting.at>
2023-03-01 10:15:51 +0100

    tset

--- server.js
+++ server.js
@@ -10,10 +10,13 @@ app.use("/", express.static("public"));
 
 app.use(express.urlencoded({ extended: false }));
 app.post("/bugreports", async (req, res) => {
-    console.log("creating bug...");
-    await prisma.bug_reports.create({ data: { bug_type: req.body.bug_type, description: req.body.description } })
-    console.log("redirecting...");
-    res.redirect("/");
+    // console.log("creating bug...");
+    // await prisma.bug_reports.create({ data: { bug_type: req.body.bug_type, description: req.body.description } })
+    // console.log("redirecting...");
+    // res.redirect("/");
+    console.log("testing");
+    console.log(req)
+    res.send("hello")
 })
 
 const port = process.env.port || 3000;
