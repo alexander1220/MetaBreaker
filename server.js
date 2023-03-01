91414ba5e30bfee951c5c72fad5d0e299c618d3c
Alex Sabani <47181191+alexander1220@users.noreply.github.com>
2023-03-01 01:30:43 +0100

    created prisma DB

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
+const bodyParser = require("body-parser");
 const express = require("express");
+const fs = require("fs");
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
 const app = express();
 
 app.use("/", express.static("public"));
+
+app.use(bodyParser.json());
+app.post("/bugreports", async (req, res) => {
+    /*
+        req:
+        {
+            "bug_type":"bug",
+            "description":"error on startup",
+        }
+    */
+    await prisma.bug_reports.create({ data: { bug_type: req.body.bug_type, description: req.body.description } })
+    res.end();
+})
 
 const port = process.env.port || 3000;
 app.listen(port, () => {
